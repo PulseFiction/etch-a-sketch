e8099ecc2ecaa1bfdb1eb3d001e1346f71d5b481
Sebastian <seb-ldn@hotmail.co.uk>
2021-10-28 11:36:39 +0100

    Initial Commit.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Etch a Sketch Project
+
+As part of the Odin Project foundtion course, I have designed an
+Etch a Sketch using mostly Javascript.
+
+Live Link:
---
+++ script.js
@@ -0,0 +1,15 @@
+
+const container = document.querySelector('.container')
+
+
+// Squares
+for (let i = 0 ; i < 256 ; i++) {
+    const div = document.createElement('div');
+    div.classList.add('square');
+    container.appendChild(div) 
+    
+}
+
+
+
+
