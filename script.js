32cc1a7ea62db3317c6da1fe41a16f33babcd0fe
Pulse Fiction <seb-ldn@hotmail.co.uk>
2021-10-30 14:04:45 +0100

    Added feature where user clicks a button to reset the grid and prompts them for a number to make a new grid out of.

--- script.js
+++ script.js
@@ -1,15 +1,108 @@
+// Globals
+const container = document.querySelector(".container");
+const btn = document.querySelector(".clear");
 
-const container = document.querySelector('.container')
 
+function sketch(){
 
-// Squares
-for (let i = 0 ; i < 256 ; i++) {
-    const div = document.createElement('div');
-    div.classList.add('square');
-    container.appendChild(div) 
+    for (let i = 0 ; i < 256 ; i++){
+        const square = document.createElement('div');
+        square.classList.add('square');
+        container.appendChild(square);
+
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = 'white'
+        })
+    }
+    
+    function reset(squares) {
+        container.innerHTML = ''
+        squares = prompt('Please enter how many squares you would like:')
+        if (squares > 100) {
+            squares = prompt('Please enter less than 100')
+        } 
+        
+        
+        for (let i = 0 ; i < squares ; i++){
+            const square = document.createElement('div');
+            square.classList.add('square');
+            container.appendChild(square);
+            
+            square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = 'white'
+            });
+        };
+
+    }
+
+    btn.addEventListener('click', reset)
     
 }
 
 
 
 
+sketch()
+/* 
+function etchSketch(squares) {
+  for (let i = 0; i < 256; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    container.appendChild(square);
+    
+    square.addEventListener("mouseover", () => {
+      square.style.backgroundColor = "#fff";
+    });
+
+  };
+  btn.addEventListener('click', () => {
+    container.removeChild(square);
+    squares = prompt('Please enter how many squares you want in the grid.')
+    if (squares > 100){
+        prompt('Please enter a number lower than 100')
+    } 
+    
+    for (let i = 0; i < squares ; i++){
+        const square = document.createElement("div");
+        square.classList.add("square");
+        container.appendChild(square);
+    }
+
+})
+  
+};
+etchSketch();
+
+
+
+
+*/
+
+// Set for loop to < user prompt, set user prompt as default 256?
+
+/* Original Code
+for (let i = 0 ; i < 256 ; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    container.appendChild(square);
+
+
+
+
+    function drawSquare() {
+        square.style.backgroundColor = '#fff'
+    };
+
+    function changeSquares(){
+        container.removeChild(square);
+        let numSquares = prompt('Please choose how many squares you would like in the grid:')
+        if (numSquares > 100) {
+            numSquares = prompt('Please choose 100 or less (This is for performance issues)')
+        }
+
+    };
+
+    btn.addEventListener('click', changeSquares);
+    square.addEventListener('mouseover', drawSquare);
+}
+*/
